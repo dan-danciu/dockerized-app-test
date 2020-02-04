e60d0075aac9f1a4a997b8162a747db5cf9377b5
dan-danciu <dan.danciu13@gmail.com>
2020-02-04 15:02:38 +0200

    moved api/docs to /apidocs to avoid the need to be authenticated while viewing docs

--- api/app/main.py
+++ api/app/main.py
@@ -4,24 +4,26 @@ from models.db import users_collection
 from routers import users
 
 
-app = FastAPI(openapi_prefix="/api",
-            title="Supernice app",
-            version="0.1.5")
+app = FastAPI(openapi_prefix="/apidocs",
+              title="Supernice app",
+              version="0.1.5")
+
 
 @app.on_event("startup")
 async def startup_event():
     users_collection.create_index(
-        [("email", pymongo.ASCENDING)], 
-        name="email_index", 
+        [("email", pymongo.ASCENDING)],
+        name="email_index",
         unique=True)
     users_collection.create_index(
-        [("first_name", "text"), 
-        ("last_name", "text")])
+        [("first_name", "text"),
+         ("last_name", "text")])
     users_collection.create_index(
-        [("username", pymongo.ASCENDING)], 
-        name="username_index", 
-        unique=True, 
-        partialFilterExpression = { "username": { "$exists": True }})
+        [("username", pymongo.ASCENDING)],
+        name="username_index",
+        unique=True,
+        partialFilterExpression={"username": {"$exists": True}})
+
 
 @app.get("/")
 def read_root():
@@ -32,5 +34,4 @@ app.include_router(
     users.router,
     prefix="/users",
     tags=["users"]
-    )
-
+)
